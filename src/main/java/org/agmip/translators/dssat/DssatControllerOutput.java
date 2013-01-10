8629f89fc78c231f567015de87168e9904966c1b
MengZhang <meng.zhang15@ufl.edu>
2013-01-10 16:44:48 -0500

    1. Fix WST_ID in XFile lost relation with weather file for when output multiple treatment

--- src/main/java/org/agmip/translators/dssat/DssatControllerOutput.java
+++ src/main/java/org/agmip/translators/dssat/DssatControllerOutput.java
@@ -84,7 +84,7 @@ public class DssatControllerOutput extends DssatCommonOutput {
                     writeSWFile(arg0, soilTmp, new DssatSoilOutput());
                     writeSWFile(arg0, wthTmp, new DssatWeatherOutput());
                     soilArrTmp.add((HashMap) soilTmp.get("soil"));
-                    wthArrTmp.add((HashMap) wthTmp.get("weather"));
+                    wthArrTmp.add(wthTmp);
                 }
                 expData.put("soil", soilArrTmp);
                 expData.put("weather", wthArrTmp);
